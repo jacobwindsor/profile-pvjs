5588823d99c2b69bc374652fc4730d971ac1064d
Jacob Windsor <jacob.windsor@ymail.com>
2017-05-15 11:38:06 +0200

    Use handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.set('views', './src');
+app.set('view engine', 'hbs');
+
+app.get('/test/old/:WPID', (req, res) => {
+  res.send(`Testing old pathway ${req.params.WPID}`);
+});
+
+app.get('/test/new/:WPID', (req, res) => {
+  res.render('test-new', { WPID: req.params.WPID });
 });
 
 app.listen(3000, () => {
